e0babb2e786c17bac78eb791bf6a779d2d8815a3
Saroj Gurung <gurungsaroj98k@gmail.com>
2023-06-08 16:15:55 +0545

    Program to be Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-export function ShowHelloWorld() {
+import { useState } from "react";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Saroj Gurung</h1>
+      <h1>Counter</h1>
+      <p>This is used to count numbers.</p>
+      <h2>Count:{count}</h2>
+      <button
+        onClick={() => {
+          console.log("State is increased", count);
+          setCount(count + 1);
+        }}
+        type="button"
+      >
+        Increase(+1)
+      </button>
+      <button
+        onClick={() => {
+          console.log("State is decreased", count);
+          setCount(count - 1);
+        }}
+        type="button"
+      >
+        Decrease(-1)
+      </button>
+      <button
+        onClick={() => {
+          console.log("State is reset", count);
+          setCount(0);
+        }}
+        type="button"
+      >
+        Reset(0)
+      </button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { ShowHelloWorld } from "./App";
+import { Counter } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ShowHelloWorld />
+    <Counter />
   </React.StrictMode>
 );
