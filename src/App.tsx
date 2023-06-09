9aeaf35c06f3e0f67a17464070c560687513f0f6
Saroj Gurung <gurungsaroj98k@gmail.com>
2023-06-09 08:56:01 +0545

    style for counter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styles from "./App.module.css";
 export function Counter() {
   const [count, setCount] = useState(0);
   return (
-    <div>
+    <div className="main">
       <style jsx>
         {`
           p,
@@ -14,34 +14,37 @@ export function Counter() {
       </style>
       <h1 style={{ textAlign: "center" }}>Counter</h1>
       <p>This is used to count numbers.</p>
-      <h2>Count:{count}</h2>
+      <h2>{count}</h2>
       <div className={styles.btnContainer}>
         <button
-          onClick={() => {
-            console.log("State is increased", count);
-            setCount(count + 1);
-          }}
-          type="button"
-        >
-          Increase(+1)
-        </button>
-        <button
+          className="decrease-btn"
           onClick={() => {
             console.log("State is decreased", count);
             setCount(count - 1);
           }}
           type="button"
         >
-          Decrease(-1)
+          -1
         </button>
         <button
+          className="reset-btn"
           onClick={() => {
             console.log("State is reset", count);
             setCount(0);
           }}
           type="button"
         >
-          Reset(0)
+          0
+        </button>
+        <button
+          className="increase-btn"
+          onClick={() => {
+            console.log("State is increased", count);
+            setCount(count + 1);
+          }}
+          type="button"
+        >
+          +1
         </button>
       </div>
     </div>
